8f8c87437bc4091def4eebdb0bb4ad3c7d18e3d2
Rudolf Luttich <rudolfluttich@gmail.com>
2019-04-11 18:10:14 +0200

    section_2 start

---
+++ intro_setup/Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string my_name = "Rudolf";
+        // string hello_line = string.Format("Hello {0}!", my_name);
+
+        print(string.Format("Hello {0}!", my_name));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
